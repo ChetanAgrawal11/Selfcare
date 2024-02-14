ab5114c2b0692cc1db6525a784931953ec791241
ChetanAgrawal11 <chetanagrawal117@gmail.com>
2024-02-14 14:29:16 +0530

    backend final changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import  { useState } from "react";
+import {  useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 const Login = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
+  const history = useNavigate();
+  const [cred, setcred] = useState({ email: "", password: "" });
+  const onchange = (e) => {
+    setcred({ ...cred, [e.target.name]: e.target.value });
+  };
+  const onsubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:5000/api/auth/login", {
+    
+      email: cred.email,
+      password: cred.password
+    });
+    
+    localStorage.setItem("token", response.data.token);
+    console.log(localStorage.getItem("token"));
+    console.log(response.data.success);
+    if(response.data.success){
+      history("/");
+    }else{
+      alert(response.data.mess);
+      history("/Login");
+    }
+    
+  };
   return (
     <div className=" bg-zinc-800 w-screen h-screen flex justify-center items-center">
-      <form action="" className="flex flex-col gap-5">
+      <form action="" className="flex flex-col gap-5" onSubmit={onsubmit}>
         <input
           type="text"
           placeholder="Email"
           className=" bg-transparent border p-2 rounded-xl text-white"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
+          name="email" value={cred.email}
+          onChange={onchange}
         />
         <input
           type="text"
           placeholder="Password"
+          name="password" value={cred.password}
           className=" bg-transparent border p-2 rounded-xl text-white"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
+          onChange={onchange}
         />
         <button
           className=" bg-blue-600 text-white rounded-3xl p-2"
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,37 +1,50 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+import  { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import axios from "axios";
 const Register = () => {
-  const [email, setEmail] = useState();
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
+  const history = useNavigate();
+  const [cred, setcred] = useState({ name: "", email: "", password: "" });
+
+ const onsubmit = async (e) => {
+   e.preventDefault();
+   const response = await axios.post("http://localhost:5000/api/auth/createUser", {
+     name: cred.name,
+     email: cred.email,
+     password: cred.password
+   });
+   localStorage.setItem("token", response.data.token);
+   history("/Login");
+ };
+ const onchange = (e) => {
+  setcred({ ...cred, [e.target.name]: e.target.value });
+};
 
   return (
     <div className=" bg-zinc-800 w-screen h-screen flex justify-center items-center">
-      <form action="" className="flex flex-col gap-5">
+      <form action="" className="flex flex-col gap-5" onSubmit={onsubmit}>
         <input
           type="text"
           placeholder="Email"
           className=" bg-transparent border p-2 rounded-xl text-white"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
+          name="email"
+          value={cred.email}
+          onChange={onchange}
         />
         <input
           type="text"
           placeholder="Username"
           className=" bg-transparent border p-2 rounded-xl text-white"
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
+          name="name"
+          value={cred.name}
+          onChange={onchange}
         />
         <input
           type="text"
           placeholder="Password"
           className=" bg-transparent border p-2 rounded-xl text-white"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
+          name="password"
+          value={cred.password}
+          onChange={onchange}
         />
         <button
           className=" bg-blue-600 text-white rounded-3xl p-2"
